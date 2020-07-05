25adf9b158cad1654765d9e077b4ec35b9d48761
yuanmeng <861013016@qq.com>
2020-07-05 14:14:37 +0800

    test:自动部署

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <h1>{{ msg }}</h1>
   <button @click="count++">count1 is: {{ count }}</button>
   <button @click="count++">count1 is: {{ count }}</button>
+  <button @click="count++">count1 is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
